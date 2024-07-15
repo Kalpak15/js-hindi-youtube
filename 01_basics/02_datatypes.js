93f0c37274f724f31390fd075eab4b55329bc4a5
Kalpak15 <collegevirat07@gmail.com>
2024-07-15 18:58:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treate all JS code as newer version
+
+// alert("Hello Everyone")   //beacuse we use node js here
+
+
+let name="Garen"
+let age=12
+let value=true
+
+console.log(typeof(name),typeof(age),typeof(value))
+
