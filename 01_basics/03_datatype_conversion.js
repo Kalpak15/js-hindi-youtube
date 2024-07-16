87c7c9963ec1977780fa2fa63efe6a944738f11b
Kalpak15 <collegevirat07@gmail.com>
2024-07-16 15:08:20 +0000

    datatypes

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,41 @@
+let score=33
+console.log(score)
+console.log(typeof score)
+
+let score1=true
+// let score1=undefined
+// let score1=null
+// let score1="33abc"
+console.log(score1)
+console.log(typeof score1)
+
+let valueInNumber=Number(score1)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+
+//"33"=>33
+// "33abc"=>NaN
+// true=>1; false=>0
+
+
+// let loggedIn=1
+// let loggedIn=""
+let loggedIn="kalpak"
+
+let booleanlogged=Boolean(loggedIn)
+
+console.log(booleanlogged)
+console.log(typeof booleanlogged)
+
+
+// 1=>true; 0=>false
+// ""=>false
+// "kalpak"=>true
+
+
+let some=33
+let stringnum=String(some)
+
+console.log(stringnum)
+console.log(typeof stringnum)
