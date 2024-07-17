698879eb1618e51088ec292d03757e2663937b1d
Kalpak15 <collegevirat07@gmail.com>
2024-07-17 14:50:14 +0000

    Operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,28 @@
+// console.log("1"+ 2)
+// console.log("1"+ "2")
+// console.log("1"+ 2+2)
+// console.log(1+ 2+"2")
+
+// console.log(+true)
+
+
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+// console.log(num1)
+
+// let game=100
+// ++game
+
+// console.log(game)
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);                                                              
